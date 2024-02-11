6a524d3a835588dc4741e575db58a9ce65cba9ff
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2024-02-11 16:07:30 -0500

    added models and seeds and started handlebars. added login and logout functionality

---
+++ seeds/userSeed.js
@@ -0,0 +1,12 @@
+const userData = [
+    {
+      username: 'Mike1234',
+      password: 'Hello1234'
+    },
+    {
+      username: 'John1234',
+      password: 'Hello1234'
+    }
+  ];
+  
+  module.exports = userData;
